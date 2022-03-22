c7fbdcffea97db85e4c1df929d57b9be8419615b
Gennaro <gennarinodi02@gmail.com>
2022-03-22 16:37:45 +0100

    bonus 1 esercizio

---
+++ bonus/main.js
@@ -0,0 +1,15 @@
+const eta1 = prompt(" Giocatore1--qual è la tua èta? ");
+const eta2 = prompt("Giocatore2--- Quale è la ua èta?")
+ 
+if(eta1>eta2)
+{
+    alert("il giocatore1 è più grande");
+}
+else if( eta2>eta1)
+{
+    alert("il giocatore2 è piu grande");
+}
+else
+{
+    alert("entrabi i giocatori hanno la stessa èta");
+}
