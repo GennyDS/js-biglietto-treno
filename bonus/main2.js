64175581b76155a4684f4c18135d6bc0ab6b228c
Gennaro <gennarinodi02@gmail.com>
2022-03-23 11:44:55 +0100

    corretto Finale

--- bonus/main2.js
+++ bonus/main2.js
@@ -2,7 +2,7 @@ const sceltaPoD = promt("scegli pari o dispari");
 const sceltaNumero =promt("inserisci un numero d 1 A 9");
 const scelta1 = parseInt(sceltaNumero);
 
-constsceltaPc = Math.floor(Math.random()*9+1);
+const sceltaPc = Math.floor(Math.random()*9+1);
 const scelta2 = parseInt(sceltaPc);
 
 const somma = scelta1 + scelta2;
